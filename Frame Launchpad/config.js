6732961dc022a22a3464fed6cb47aba6a3387778
nashv <nash@vartak.org>
2018-01-24 22:34:43 +0100

    Add files via upload

---
+++ Frame Launchpad/config.js
@@ -0,0 +1,11 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "Frame Launchpad",
+   "homepage": "https://app.fra.me",
+   "enableNavBttns": false,
+   "enableHomeBttn": false,
+   "enableReloadBttn": false,
+   "enableLogoutBttn": false,
+   "kioskEnabled": false
+};
